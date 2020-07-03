88dad00adfcd32809e750b24e56a2ef0452dd714
luzh <332862394@qq.com>
2020-07-03 12:33:34 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,74 @@
+let html = document.querySelector("#html");
+let style = document.querySelector("#style");
+
+let string = `/*你好，这是测试
+*接下来我演示一下我的前端功底
+*首先我要准备一个div
+**/
+#div1{
+    border:1px solid red;
+    width:200px;
+    height:200px;
+}
+/*接下来我要把div变成一个八卦图
+*注意看好了
+*首先把div变成一个圆
+**/
+#div1{
+    border-radius:50%;
+    box-shadow:0 0 3px rgba(0,0,0,0.5);
+    border:none;
+}
+/* 变颜色
+**/
+#div1{
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+/*加两个圆球**/
+#div1::before{
+    width:100px;
+    height:100px;
+    top:0;
+    left:50%;
+    transform:translateX(-50%);
+    background:#000;
+    border-radius:50%;
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%);
+}
+#div1::after{
+    width:100px;
+    height:100px;
+    bottom:0;
+    left:50%;
+    transform:translateX(-50%);
+    background:#fff;
+    border-radius:50%;
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%);
+}
+`;
+// string = string.replace(/\n/g, "<br>");
+let string2 = "";
+let n = 0;
+let step = () => {
+  setTimeout(() => {
+    //如果是回车就不照搬
+    //如果不是回车，就照搬
+    if (string[n] === "\n") {
+      string2 += "<br>";
+    } else if (string[n] === " ") {
+      string2 += "&nbsp;";
+    } else {
+      string2 += string[n];
+    }
+    html.innerHTML = string2;
+    style.innerHTML = string.substring(0, n);
+    window.scrollTo(0, 999999);
+    html.scrollTo(0, 999999);
+    if (n < string.length - 1) {
+      n += 1;
+      step();
+    } else {
+    }
+  }, 50);
+};
+step();
